a6f24d14a52a52e58bd26f168a77940acc2e7c10
Nitin V3muri <nitinvemuri@gmail.com>
2021-08-07 11:45:53 -0700

    ?

---
+++ src/components/Resume/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../../App.css";
+
+function Resume() {
+    return (
+        <div>
+           I am clearly changing something why is the repo not wokring please tell me
+        </div>
+    )
+}
+
+export default Resume;
